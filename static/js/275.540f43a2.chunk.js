8cbc5c81010ec0c867e607dc3815ceda7602e150
VitaKoval <106755156+VitaKoval@users.noreply.github.com>
2022-12-07 09:18:34 +0000

    Deploying to gh-pages from @ VitaKoval/movies-react@fb9a9b11df0e0f53cac9679dcbd280a915b723a3 🚀

---
+++ static/js/275.540f43a2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[275],{8275:function(e,t,r){r.r(t);var c=r(7689),n=r(184);t.default=function(){return(0,n.jsx)("section",{children:(0,n.jsx)(c.j3,{})})}}}]);
+//# sourceMappingURL=275.540f43a2.chunk.js.map
